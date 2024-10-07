8b5622f158be0d9b94cb18b748e8776f84f6e61c
maniganiga <enkhmunkh.e1@orkhonschool.edu.mn>
2024-10-07 20:17:04 +0800

    commition

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import MyComp from "./components/MyComp";
+
+function App() {
+  return (
+    <div>
+      <MyComp />
+      <MyComp />
+      <MyComp />
+      <MyComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyComp.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MyComp = () => {
+  return (
+    <div className="hairtsag">
+      <div>
+        <h1 className="commerce">MyComp</h1>
+      </div>
+      <div>
+        <h1 className="commerce">MyComp</h1>
+      </div>
+      <div>
+        <h1 className="commerce">MyComp</h1>
+      </div>
+    </div>
+  );
+};
+
+export default MyComp;
